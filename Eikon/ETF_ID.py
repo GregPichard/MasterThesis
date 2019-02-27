8ec52fa19e8e47f703ffae384a95d23f8fb3b8a7
Greg <greg.pichard@gmail.com>
2019-02-27 09:46:47 +0100

    Eikon Python API bug

--- Eikon/ETF_ID.py
+++ Eikon/ETF_ID.py
@@ -5,11 +5,13 @@ eikon.set_app_key('638fa3bbb90349d5a97dc60c1c0cc4b0b5646846')
 
 
 ETF_db = pd.read_excel("ReportEikon_ETF_20180224.xlsx", header = 0)
-print(ETF_db.head())
-print(ETF_db[['Lipper RIC']].nunique()) #All different
-print(ETF_db[['Lipper RIC']].dropna().nunique())# Same length, thus no NA.
+#print(ETF_db.head())
+#print(ETF_db[['Lipper RIC']].nunique()) #All different
+#print(ETF_db[['Lipper RIC']].dropna().nunique())# Same length, thus no NA.
+ric_list = np.array((ETF_db[['Lipper RIC']]))
+#print(str(ric_list[0]))
+for ric in ric_list[0:2]:
+    print(ric[0])
+    Constituents, err = eikon.get_data(ric[0], ['TR.IndexConstituentRIC', {'TR.IndexConstituentWeightPercent':{'sort_dir':'desc'}}, 'TR.PctOfSharesOutHeld'], {'SDate':'2019-01-31'})
+    print(Constituents)
 
-for ric in ETF_db[['Lipper RIC']].loc([1:3]):
-    #Constituents, err = eikon.get_data(ric, ['TR.IndexConstituentRIC', {'TR.IndexConstituentWeightPercent':{'sort_dir':'desc'}}], {'SDate':'2019-02-01'})
-    #print(Constituents)
-    print(ric)
