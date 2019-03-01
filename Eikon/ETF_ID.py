86401155a67c8e3c57c7697dc131c738d0470828
Greg <greg.pichard@gmail.com>
2019-03-01 10:51:03 +0100

    Modified login process using eikon library

--- Eikon/ETF_ID.py
+++ Eikon/ETF_ID.py
@@ -1,17 +1,21 @@
 import numpy as np
 import pandas as pd
 import eikon
-eikon.set_app_key('638fa3bbb90349d5a97dc60c1c0cc4b0b5646846')
+import configparser as cp
+cfg = cp.ConfigParser()
+cfg.read('eikon.cfg')
+eikon.set_app_key(cfg['eikon']['app_id'])
 
 
 ETF_db = pd.read_excel("ReportEikon_ETF_live_20190228.xlsx", header = 0)
 #print(ETF_db.head())
 #print(ETF_db[['Lipper RIC']].nunique()) #All different
 #print(ETF_db[['Lipper RIC']].dropna().nunique())# Same length, thus no NA.
-ric_list = np.array((ETF_db[['Lipper RIC']]))
-#print(str(ric_list[0]))
-for ric in ric_list[0:3]:
-    print(ric[0])
-    Constituents, err = eikon.get_data(ric[0], ['TR.IndexConstituentRIC', {'TR.IndexConstituentWeightPercent':{'sort_dir':'desc'}}, 'TR.PctOfSharesOutHeld'], {'SDate':'2019-01-31'})
+ric_list = ETF_db.Lipper_RIC.tolist()
+#print(ric_list)
+for ric in ric_list[0:1]:
+    #print(ric)
+    Constituents, err = eikon.get_data(ric, ['TR.IndexConstituentRIC', 'TR.PctOfSharesOutHeld'], {'SDate':['2019-01-31', '2018-12-31']})
     print(Constituents)
-
+#Constituents, err = eikon.get_data(ric_list[0], 'TR.IndexConstituentRIC', {'SDate':'2019-01-31'})
+#print(Constituents)
