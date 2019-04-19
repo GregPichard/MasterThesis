8f6d1912e04c5730cd1236d7e439e05c9092cc91
GregPichard <greg.pichard@gmail.com>
2019-04-19 15:06:34 +0200

    Adapted for international stocks

---
+++ Eikon/IntlStockFundOwnership_monthly.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 12:31:08 2019
+Adapted from StockFundOwnership_monthly.py for international (i.e. non-US stocks)
+
+@author: gpichard
+"""
+if __name__ == "__main__":
+    import os
+    import numpy as np
+    import pandas as pd
+    import datetime as dt
+    import eikon
+    import configparser as cp
+    cfg = cp.ConfigParser()
+    cfg.read('eikon.cfg')
+    eikon.set_app_key(cfg['eikon']['app_id'])
+
+    from itertools import compress
+    import multiprocessing
+
+def Concat_Stocks(path):
+    region_files_list = os.listdir(path)
+    Stocks_db = list()
+    for f in region_files_list:
+        region_file_db = pd.read_excel(path + f, header = 0)
+        Stocks_db.append(region_file_db)
+    Stocks_db = pd.concat(Stocks_db, sort = False)
+    return Stocks_db
+
+def Get_Data(eikon_iter_ric_list, date):
+    FundOwners, err = eikon.get_data(eikon_iter_ric_list, ['TR.FundInvestorType', 'TR.FundHoldingsDate', 'TR.FundPortfolioName', 'TR.FundClassID', 'TR.FundAdjShrsHeld'], {'SDate':date})
+    return FundOwners
+
+def Loop_Stocks(ric_list, date):
+    N_stocks = len(ric_list)
+    print("Number of stocks : ", N_stocks)
+    initial_value = 0
+    ideal_width = 50
+    width = ideal_width
+    while initial_value < N_stocks:
+        if width == 0:
+            initial_value += 1
+            width = ideal_width
+        if initial_value + width - 1 >= N_stocks:
+            end_value = None 
+        else:
+            end_value = initial_value + width
+        eikon_iter_ric_list = ','.join(ric_list[initial_value:end_value])
+        #print(eikon_iter_ric_list)
+        try:
+            FundOwners = Get_Data(eikon_iter_ric_list, date)
+            #print(FundOwners)
+            FundOwners = FundOwners.loc[FundOwners['Lipper Primary Fund Class ID'].notnull() & (FundOwners['Fund Shares Held (Adjusted)'] > 0)]
+            #print(FundOwners)
+            FundOwners.to_csv("Monthly/IntlFundOwners_"+date+"_db.csv", mode = 'a', header = False)
+            print("init", initial_value, "end", end_value, "-> OK !")
+            initial_value += width
+            width = ideal_width
+        except:
+            width //= 4
+            #attempt_count += 1
+            #print("Additional attempt(s): ", attempt_count)
+            #try :
+                #FundOwners = Get_Data(eikon_iter_ric_list, date)
+            #except:
+                #attempt_count += 1
+                #print("Additional attempt(s): ", attempt_count)
+                #try:
+                    #FundOwners = Get_Data(eikon_iter_ric_list, date)
+                #except:
+                # print("Could not succeed after ", attempt_count, " attempts. Init value: ", initial_value, ". Width . ", width)
+
+
+def main():
+    Stocks_db = Concat_Stocks('./NonUS_StocksLists/')
+    ric_list = Stocks_db.RIC.tolist()
+
+    #eikon_ric_list = ','.join(ric_list[1000:1001])
+    #print(ric_list)
+    mo = range(1, 13)
+    dd = list([31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31])
+    years = range(1999, 2019, 1)
+    ref_dates = list()
+    dates_list = list()
+    for y in years:
+        for m, month in enumerate(mo):
+            dates_list.append(str(y) + "-" + str(month).zfill(2) + "-" + str(dd[m]))
+            ref_dates.append(dt.datetime(y, month, dd[m], 0, 0))
+    for i, date in enumerate(dates_list):
+        #p = multiprocessing.Pool(processes = multiprocessing.cpu_count()) # - 1 if more than one CPU
+        #print("Number of CPU cores used : ", multiprocessing.cpu_count()) # - 1
+        print("Processing date : ", date)
+        print(ref_dates[i])
+        stocks_kept = [None for x in range(len(ric_list))]
+        for j in range(0, len(ric_list)):
+            if isinstance(Stocks_db.Date_Became_Public.iat[j], dt.datetime):
+                stocks_kept[j] = Stocks_db.Date_Became_Public.iat[j] < ref_dates[i]
+            else:
+                stocks_kept[j] = True
+                #print(list(compress(ric_list, stocks_kept)))
+        print(date)
+        Loop_Stocks(list(compress(ric_list,stocks_kept)), date)
+        print("Processed date : ", date)
+        #p.close()
+        #p.join()
+
+main()
+
---
+++ Eikon/IntlStocksPrice-volume_Series_daily_query.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 19:16:43 2019
+Adapted from OutstandingShares_Monthly_query.py
+@author: Grégoire
+"""
+
+if __name__ == "__main__":
+    import pandas as pd
+    import numpy as np
+    import datetime as dt
+    import eikon
+    import configparser as cp
+    cfg = cp.ConfigParser()
+    cfg.read('eikon.cfg')
+    eikon.set_app_key(cfg['eikon']['app_id'])
+    
+def Get_Data(iter_ric_list):
+    # The sample has to be cut into two bits : from August 1999 to late January 2007 and from February 2007 to end 2018.
+    #TS = eikon.get_timeseries(iter_ric_list, fields = ['TIMESTAMP','VOLUME', 'HIGH', 'LOW', 'OPEN', 'CLOSE'], start_date='1999-08-01', end_date='2007-01-31', interval='daily', normalize=True)
+    TS = eikon.get_timeseries(iter_ric_list, fields = ['TIMESTAMP','VOLUME', 'HIGH', 'LOW', 'OPEN', 'CLOSE'], start_date='2007-02-01', end_date='2018-12-31', interval='daily', normalize=True)
+    return TS
+
+def Loop_Stocks(ric_list):
+    N_stocks = len(ric_list)
+    print("Number of stocks : ", N_stocks)
+    initial_value = 0
+    ideal_width = 1
+    width = ideal_width
+    while initial_value < N_stocks:
+        if width == 0:
+            initial_value += 1
+            width = ideal_width
+        if initial_value + width - 1 >= N_stocks:
+            end_value = None 
+        else:
+            end_value = initial_value + width
+        iter_ric_list = ric_list[initial_value:end_value]
+        #print(eikon_iter_ric_list)
+        try:
+            TS = Get_Data(iter_ric_list)
+            #print(TS)
+            TS.to_csv("Daily/IntlStockPrice-volume_Series_daily_db.csv", mode = 'a', header = False)
+            #TS.to_hdf("Monthly/AdditionalOutstandingShares_Stocks_Monthly_db.hdf", key = 'out_shares', complevel = 6, complib = 'zlib')
+            #FundOwners.to_sql('FundOwners_db', engine, if_exists = 'append', index = True, index_label = "Instrument")
+            print("init", initial_value, "end", end_value, "-> OK !")
+            initial_value += width
+            width = ideal_width
+        except:
+            width //= 2
+
+def main():
+    StocksRICs = np.load("IntlFundOwners_ETFHeld_RIC_list.npz") # Original query for the whole list of stocks
+    StocksRICs = list(StocksRICs['RIC']) 
+#    StocksRICs = np.load("Monthly/Additional41_RIC_list.npz")
+#    StocksRICs = list(StocksRICs['arr_0'])
+ 
+    Loop_Stocks(StocksRICs)
+
+ 
+main()
