e2d0ab5150ae9ae0f841fc7bab52ca47edfded02
Grégoire Pichard <greg.pichard@gmail.com>
2019-06-04 16:26:47 +0200

    Adapted query script for missing daily price/volume

---
+++ Eikon/StocksPrice-volume_Missing_Series_daily_query.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 16:24:01 2019
+Adapted from StocksPrice-volume_Series_daily_query.py
+@author: Grégoire
+"""
+
+if __name__ == "__main__":
+    import pandas as pd
+    import numpy as np
+    import datetime as dt
+    import eikon
+    import configparser as cp
+    cfg = cp.ConfigParser()
+    cfg.read('eikon.cfg')
+    eikon.set_app_key(cfg['eikon']['app_id'])
+    import time
+    
+def Get_Data(iter_ric_list, iter_end_dates):
+    # The sample has to be cut into two bits : from August 1999 to late January 2007 and from February 2007 to end 2018.
+    TS = eikon.get_timeseries(iter_ric_list, fields = ['TIMESTAMP','VOLUME', 'HIGH', 'LOW', 'OPEN', 'CLOSE'], start_date='1999-08-01', end_date=iter_end_dates[0], interval='daily', normalize=True)
+    #TS = eikon.get_timeseries(iter_ric_list, fields = ['TIMESTAMP','VOLUME', 'HIGH', 'LOW', 'OPEN', 'CLOSE'], start_date='2007-02-01', end_date='2018-12-31', interval='daily', normalize=True)
+    return TS
+
+def Loop_Stocks(ric_list, end_dates_series):
+    N_stocks = len(ric_list)
+    print("Number of stocks : ", N_stocks)
+    initial_value = 0
+    ideal_width = 1
+    width = ideal_width
+    while initial_value < N_stocks:
+        if width == 0:
+            initial_value += 1
+            width = ideal_width
+        if initial_value + width - 1 >= N_stocks:
+            end_value = None 
+        else:
+            end_value = initial_value + width
+        iter_ric_list = ric_list[initial_value:end_value]
+        iter_end_dates = list(end_dates_series.filter(iter_ric_list))
+        #print(eikon_iter_ric_list)
+        try:
+            time.sleep(1)
+            TS = Get_Data(iter_ric_list, iter_end_dates)
+            #print(TS)
+            TS.to_csv("D:/ETF_GP/Daily/StockPrice-volume_Series_daily_db.csv", mode = 'a', header = False)
+            #TS.to_hdf("Monthly/AdditionalOutstandingShares_Stocks_Monthly_db.hdf", key = 'out_shares', complevel = 6, complib = 'zlib')
+            #FundOwners.to_sql('FundOwners_db', engine, if_exists = 'append', index = True, index_label = "Instrument")
+            print("init", initial_value, "end", end_value, "-> OK !")
+            initial_value += width
+            width = ideal_width
+        except:
+            width //= 2
+
+def main():
+    #StocksRICs = pd.read_excel("ReportEikon_Stocks_US_static_20190304.xlsx", header = 0)
+    #StocksRICs = StocksRICs.RIC.tolist()
+#    # Initial error of not including the RICs of companies that never held ETF. Here they are (early comparison just after noticing the mistake) :
+#    RemainingRICs = list(set(StocksRICs).difference(set(pd.read_csv('D:/ETF_GP/Daily/StockPrice-volume_Series_daily_db.csv', header = None, usecols = [2], squeeze= True).unique())))
+#    # Manually processed the list to a CSV file :
+#    pd.Series(RemainingRICs).to_csv('RemainingRICs_StocksPrice-volume_Series.csv')
+    # Unfortunately, the script has to be run twice because of subperiods. Read the record of missing RICs :
+    #RemainingRICs = list(pd.read_csv('RemainingRICs_StocksPrice-volume_Series.csv', header = None, usecols= [1], squeeze = True))
+    ## Missing stocks : the actual use of this script
+    MissingStocks = pd.read_csv('Issues/MissingDailyPriceVol_timetable.csv', header = 0, index_col = 0) # Do not parse dates as such : the string format is needed in the request
+    StocksRICs = list(MissingStocks.index)
+    Loop_Stocks(StocksRICs, MissingStocks.Earliest)
+    #Loop_Stocks(RemainingRICs)
+    #Loop_Stocks(StocksRICs)
+
+if __name__ == "__main__": 
+    main()
